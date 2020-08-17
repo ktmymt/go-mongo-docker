db1f2b215184a82dcd3fb5a91dcc06ce8e492656
kotaMiya <kota.miyamoto18@gmail.com>
2020-08-18 00:41:20 +0900

    connect db

--- main.go
+++ main.go
@@ -1,27 +1,48 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go-mongo-docker/configs"
+	"go-mongo-docker/domain/repository"
+	"go-mongo-docker/services"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 
+	err := godotenv.Load()
+
+	if err != nil {
+		panic(err)
+	}
+
+	config := configs.GetConfig()
+
+	options := options.Client().ApplyURI(config.MongoDB.URI)
+
+	mongodb, err := mongo.Connect(context.Background(), options)
+
+	if err != nil {
+		panic(err)
+	}
+
+	todoRepo := repository.NewTodoRepository(mongodb)
+
+	todoService := services.TodoService(todoRepo)
+
+	fmt.Println(todoRepo, todoService)
+
 	r.GET("/", func(cxt *gin.Context) {
 		cxt.JSON(http.StatusOK, gin.H{
 			"message": "Hello world",
 		})
 	})
-
-	// r.GET("/todo", func(cxt *gin.Context) {
-	// 	cxt.JSON(http.StatusOK, gin.H{
-	// 		"todo": {
-	// 			"title": "this is a title",
-	// 			"description": "this is a description"
-	// 		},
-	// 	})
-	// })
 
 	r.Run()
 }
