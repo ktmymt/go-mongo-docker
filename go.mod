9c1aa16204f4101c78a07b98c29bc9bb1a61644d
kotaMiya <kota.miyamoto18@gmail.com>
2020-08-17 23:45:14 +0900

    change module

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module server
+module go-mongo-docker
 
 go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/joho/godotenv v1.3.0
+	go.mongodb.org/mongo-driver v1.4.0
 )
--- main.go
+++ main.go
@@ -14,5 +14,14 @@ func main() {
 		})
 	})
 
+	// r.GET("/todo", func(cxt *gin.Context) {
+	// 	cxt.JSON(http.StatusOK, gin.H{
+	// 		"todo": {
+	// 			"title": "this is a title",
+	// 			"description": "this is a description"
+	// 		},
+	// 	})
+	// })
+
 	r.Run()
 }
