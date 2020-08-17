e2512665889e0779b93449e0be9eabcc947e88bf
kotaMiya <kota.miyamoto18@gmail.com>
2020-08-18 01:13:43 +0900

    add service

---
+++ services/todo_service.go
@@ -0,0 +1,28 @@
+package services
+
+import (
+	"go-mongo-docker/domain/entity"
+	"go-mongo-docker/domain/repository"
+)
+
+// TodoService interface
+type TodoService interface {
+	CreateTodo(todo *entity.Todo) (*entity.Todo, error)
+}
+
+type todoService struct {
+	Repo repository.Repository
+}
+
+// NewTodoService return service
+func NewTodoService(
+	repo repository.Repository,
+) TodoService {
+	return &todoService{
+		Repo: repo,
+	}
+}
+
+func (ts *todoService) CreateTodo(todo *entity.Todo) (*entity.Todo, error) {
+	return ts.Repo.CreateTodo(todo)
+}
