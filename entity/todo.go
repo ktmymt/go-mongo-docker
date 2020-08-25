d7819e6da9b46db9430b82dce0d41f0030797615
kotaMiya <kota.miyamoto18@gmail.com>
2020-08-26 04:07:05 +0900

    delete id

--- entity/todo.go
+++ entity/todo.go
@@ -1,12 +1,7 @@
 package entity
 
-import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
-
 // Todo entity has title and description
 type Todo struct {
-	ID          primitive.ObjectID `bson:"_id" json:"id,omitempty"`
-	Title       string             `json:"title"`
-	Description string             `json:"description"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
