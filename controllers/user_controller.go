0c807371c64bcb649466fc8f167f8edc3cff8459
Myrark <myrark.rohma@gmail.com>
2021-06-27 01:52:48 -0700

    create method outline

---
+++ controllers/user_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"go-mongo-docker/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController interface {
+	GetOwnProjects(*gin.Context)
+}
+
+type userController struct {
+	us services.UserService
+}
+
+//
+func NewUserController(us services.UserService) UserController {
+	return &userController{
+		us: us,
+	}
+}
+
+func (ctl *userController) GetOwnProjects(ctx *gin.Context) {
+	//TODO
+}
