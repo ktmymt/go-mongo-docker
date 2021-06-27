1fe4cb730c4d97c2bdbd6ebdb91e5a15db59b282
Myrark <myrark.rohma@gmail.com>
2021-06-27 01:32:53 -0700

    create method outline

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,1 +1,30 @@
 package repository
+
+import (
+	"go-mongo-docker/entity"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserRepository interface {
+	GetOwnProjects() ([]*entity.Project, error)
+}
+
+type userRepository struct {
+	db *mongo.Client
+}
+
+// NewProjectRepository returns "ProjectRepository"
+func NewUserRepository(db *mongo.Client) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+/**
+ * @summary: gets projects by user id
+ * @return : projects, error
+ */
+func (ur *userRepository) GetOwnProjects() ([]*entity.Project, error) {
+	return nil, nil
+}
