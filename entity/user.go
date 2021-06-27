6a002c85e2adb4e675ff293d59f8f3559802ed3d
Myrark <myrark.rohma@gmail.com>
2021-06-26 23:28:57 -0700

    created user validation

--- entity/user.go
+++ entity/user.go
@@ -7,3 +7,20 @@ type User struct {
 	Username string             `json:"username"`
 	Email    string             `json:"email"`
 }
+
+func (user *User) Validation(errors Errors, errorMessage ErrorMessage) Errors {
+
+	if len(user.Username) == 0 {
+		errorMessage.Name = "Username"
+		errorMessage.Message = "Username is required"
+		errors.Errors = append(errors.Errors, errorMessage)
+	}
+
+	if len(user.Email) == 0 {
+		errorMessage.Name = "Email"
+		errorMessage.Message = "Email is required"
+		errors.Errors = append(errors.Errors, errorMessage)
+	}
+
+	return errors
+}
