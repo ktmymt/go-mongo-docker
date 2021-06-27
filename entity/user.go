d77350c6f5d05630ac8a4f982f0df792a8084d45
Myrark <myrark.rohma@gmail.com>
2021-06-26 23:20:03 -0700

    created user entity

---
+++ entity/user.go
@@ -0,0 +1,9 @@
+package entity
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	Id       primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	Username string             `json:"username"`
+	Email    string             `json:"email"`
+}
