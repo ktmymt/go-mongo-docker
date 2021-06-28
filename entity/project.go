d72477bab5274baf0ed5822e67b1899fc4282556
Myrark <myrark.rohma@gmail.com>
2021-06-28 14:18:36 -0700

    update entity

--- entity/project.go
+++ entity/project.go
@@ -11,6 +11,7 @@ import (
 // Id, Name, Description, Todos, Color, and UpdateDate?
 type Project struct {
 	Id          primitive.ObjectID `bson:"_id" json:"id,omitempty"`
+	UserId      primitive.ObjectID `json:"userId"`
 	Name        string             `json:"name"`
 	Description string             `json:"description"`
 	Todos       []Todo             `json:"todos"`
