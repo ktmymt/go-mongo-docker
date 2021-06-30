bca3b418d247cc2fd2d6294b4f654066926973d0
Myrark <myrark.rohma@gmail.com>
2021-06-30 06:53:07 -0700

    get projects by user id

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -26,6 +26,30 @@ func NewUserRepository(db *mongo.Client) UserRepository {
 	}
 }
 
+func createNewUser(ur *userRepository, username string, email string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	collection := ur.db.Database("taski").Collection("users")
+	insert := bson.D{
+		{Key: "username", Value: username},
+		{Key: "email", Value: email},
+	}
+
+	incompleteInsertion, err := collection.InsertOne(ctx, insert)
+	avoidPanic(err)
+
+	autoIncrementedId := incompleteInsertion.InsertedID.(primitive.ObjectID)
+	filter := bson.M{"_id": autoIncrementedId}
+	update := bson.M{
+		"$set": bson.M{
+			"id": autoIncrementedId,
+		}}
+
+	_, err = collection.UpdateOne(ctx, filter, update)
+	avoidPanic(err)
+}
+
 /**
  * @summary: gets projects by user id
  * @return : projects, error
@@ -33,47 +57,23 @@ func NewUserRepository(db *mongo.Client) UserRepository {
 func (ur *userRepository) GetOwnProjects(userId string, username string, email string) ([]*entity.Project, error) {
 
 	if userId == "" {
-		// register user
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancel()
-
-		collection := ur.db.Database("taski").Collection("users")
-		insert := bson.D{
-			{Key: "username", Value: username},
-			{Key: "email", Value: email},
-		}
-
-		incompleteInsertion, err := collection.InsertOne(ctx, insert)
-		avoidPanic(err)
-
-		autoIncrementedId := incompleteInsertion.InsertedID.(primitive.ObjectID)
-		filter := bson.M{"_id": autoIncrementedId}
-		update := bson.M{
-			"$set": bson.M{
-				"id": autoIncrementedId,
-			}}
-
-		_, err = collection.UpdateOne(ctx, filter, update)
-		avoidPanic(err)
-
+		createNewUser(ur, username, email)
 		return nil, nil
-
 	}
 
+	// get projects
 	projectCollection := ur.db.Database("taski").Collection("projects")
-
 	projectFilter := options.Find()
 	projectFilter.SetSort(bson.D{{Key: "updatedAt", Value: -1}})
 	projectFindResult, err := projectCollection.Find(context.Background(), bson.D{}, projectFilter)
 	avoidPanic(err)
 
 	var projects []*entity.Project
-
 	for projectFindResult.Next(context.Background()) {
 		var projcet *entity.Project
+		err := projectFindResult.Decode(&projcet)
+		avoidPanic(err)
 		if projcet.UserId == convertToObjectId(userId) {
-			err := projectFindResult.Decode(&projcet)
-			avoidPanic(err)
 			projects = append(projects, projcet)
 		}
 	}
